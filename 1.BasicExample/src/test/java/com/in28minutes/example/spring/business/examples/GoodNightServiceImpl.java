1ce323aaf1ef5d7d329a7eeb44219ea0fb5293a5
Samuel Tan <samueltango@gmail.com>
2018-05-27 09:30:42 -0400

    Initial setup

---
+++ 1.BasicExample/src/test/java/com/in28minutes/example/spring/business/examples/GoodNightServiceImpl.java
@@ -0,0 +1,11 @@
+package com.in28minutes.example.spring.business.examples;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoodNightServiceImpl implements HiService {
+	@Override
+	public String sayHi() {
+		return "Good Night";
+	}
+}
